ab5040179180a785de2c81d834480be43ca9e035
pterik <pterik@gmail.com>
2023-06-23 15:01:22 -0400

    Update fj_connection_remote.php

--- public_html/fj_connection_remote.php
+++ public_html/fj_connection_remote.php
@@ -1,7 +1,7 @@
 <?php
 Class Connection {
-private  $server = "mysql:host=ka455770.mysql.tools;dbname=ka455770_jobsearch";
-private  $user = "ka455770_jobsearch";
+private  $server = "mysql:host=mysql.tools;dbname=";
+private  $user = "";
 private  $pass = "";
 private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 protected $con;
